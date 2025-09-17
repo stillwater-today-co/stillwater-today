4bfdf50d8e2ccdd97c1a726cc910eda56b8cfa40
BuffyLeo <ziyu.huang@okstate.edu>
2025-09-17 16:11:55 +0000

    database

---
+++ database.sql
@@ -0,0 +1,17 @@
+create table user {
+userID int primary key,
+userName varchar (50),
+userPassword varchar (50),
+
+
+}
+
+create table weather {
+weatherID int primary key,
+weatherType varchar (30),
+}
+
+create table events {
+eventID int primary key,
+
+}
