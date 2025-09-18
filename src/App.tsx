bac4b946c441bfdf4cfab411d7fff8e4ad281b69
bentleylewis <bbentleylewis@gmail.com>
2025-09-18 10:49:44 -0500

    PBI-001: Scaffold React + Typescript project with folder structure

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import './App.css'
+
+function App() {
+  return (
+    <div>
+      <h1> Stillwater Today</h1>
+      <p> React + Typescript is running!</p>
+    </div>
+  )
+}
+
+export default App
