e6facbbaab9430fd903961a31cd601e051e26806
Bentley Lewis <144868850+bentleylewis@users.noreply.github.com>
2025-09-19 12:34:47 -0500

    Import issues (#87)
    * fix import issues
    
    * import issue

--- src/firebase/index.ts
+++ src/firebase/index.ts
@@ -1,5 +1,5 @@
 // src/firebase/index.ts
-export { app, analytics } from "./config";
+export { app } from "./config";
 export { firestore } from "./firestore";
 export { auth } from "./auth";
 export { storage } from "./storage";
