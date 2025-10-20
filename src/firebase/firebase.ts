c2ce92df6bb197e9b99c4189360e5244484f8f4d
bentleylewis <bbentleylewis@gmail.com>
2025-10-20 09:55:19 -0500

    firebase issue

--- src/firebase/firebase.ts
+++ src/firebase/firebase.ts
@@ -23,8 +23,3 @@ const app = getApps().length === 0 ? initializeApp(firebaseConfig) : getApps()[0
 export const auth = getAuth(app);
 export const db = getFirestore(app);
 
-// Analytics only in the browser
-export let analytics;
-if (typeof window !== "undefined") {
-  analytics = getAnalytics(app);
-}
